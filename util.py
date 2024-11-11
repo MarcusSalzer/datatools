2e03fcef21cde766d6403c5bdc42f68778ecc6d3
Marcus Salzer <marcus.salzer@hotmail.se>
2024-11-11 12:40:16 +0100

    init

---
+++ util.py
@@ -0,0 +1,23 @@
+import regex as re
+
+
+def normalize_names(
+    names: list[str],
+    wordlength: int | None = None,
+    maxwords: int | None = None,
+):
+    result = []
+    for n in names:
+        words = re.split("[ \|,\._-]", n.lower().strip())
+
+        if wordlength is not None:
+            words = [w[:wordlength] for w in words]
+        if maxwords is not None:
+            words = words[:maxwords]
+
+        n = "_".join(words)
+        result.append(n)
+    if len(set(result)) != len(result):
+        raise ValueError("processed names not unique")
+
+    return result
